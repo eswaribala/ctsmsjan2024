a6fbdbfd110677115f3eb58272b56e72f8a0df2b
Parameswari Bala <parameswaribala@gmail.com>
2024-02-19 10:57:19 +0530

    updated

--- gooddayapp/gooddayapp/src/main/java/com/cts/gooddayproduct/gooddayapp/controllers/ProductController.java
+++ gooddayapp/gooddayapp/src/main/java/com/cts/gooddayproduct/gooddayapp/controllers/ProductController.java
@@ -3,10 +3,15 @@ package com.cts.gooddayproduct.gooddayapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cts.gooddayproduct.gooddayapp.models.Product;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class ProductController {
 	//url handler
 	@GetMapping("/home")
@@ -15,6 +20,12 @@ public class ProductController {
 		model.addAttribute("product", new Product());
 		return "home.html";
 		
+	}
+	@PostMapping("/save")
+	public String saveProduct(@ModelAttribute("product") Product product, Model model) {
+		
+		log.info(""+product.getItemCode());
+		return "showProducts.html";
 	}
 	
 
